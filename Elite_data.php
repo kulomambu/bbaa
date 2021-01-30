72e8df3fc3452228e2dd29eab52bba1a1e4c47ed
kulomambu <77854129+kulomambu@users.noreply.github.com>
2021-01-30 10:09:58 +0700

    Add files via upload

---
+++ Elite_data.php
@@ -0,0 +1,20 @@
+<?php
+
+
+/* CONFIG BY FChyd
+SUBSCRIBE CHANNEL AUTHOR E. SCRIPTER
+COPYRIGHT 2020
+ILEGAL TO MODIFY / DECRYPT THIS SCRIPT
+THANKS
+*/
+
+
+# DATA LOGIN
+$email = "syahrulray2@gmail.com";
+
+$password = "zxcvcxz123";
+
+
+
+# SEARCH IN GOOGLE "MY USER AGENT"
+$useragent = "Mozilla/5.0 (Linux; Android 9; ASUS_X00TD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
